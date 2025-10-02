e88db1f68316ad631dbde8b6fe442f76e3743d8b
Jason <jasn74939@gmail.com>
2025-10-02 17:08:47 -0400

    idk what to name this comit other than fuck vs code

--- src/app/model.py
+++ src/app/model.py
@@ -3,3 +3,8 @@ from utils import dotenv_values, OpenAI
 # load api
 config = dotenv_values(".env")
 client = OpenAI(api_key=config["OPENAI_API_KEY"])
+
+response = client.responses.create(
+    model="gpt-5",
+    input=user_input
+)
--- src/app/routes.py
+++ src/app/routes.py
@@ -1,21 +1,13 @@
-from utils import Flask, requests, jsonify, render_template
+from flask import request, jsonify, render_template
+from . import app, client
 
-# homepage
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# get data from js
-@app.route("/chat", mehods=["post"])
+@app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
     user_input = data.get("message", "")
 
-# send response
-return jsonify({"reply": reponse.output_text})
-
-# talk to api
-reponse = client.reponses.create(
-    model="gpt-5"
-    input="user_input"
-)
+    return jsonify({"reply": response.output_text})
